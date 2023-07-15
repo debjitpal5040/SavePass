2494defa85bd330a789cc1820b2f7eb6fe71c733
Debjit Pal <debjitpal5040@gmail.com>
2023-07-15 11:31:48 +0530

    upgrade

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # SavePass
+
 A password manager application made using python tkinter
 
 ## Start
 
-macOs             |  Windows
-:-------------------------:|:-------------------------:
-![](https://user-images.githubusercontent.com/76846542/152035198-70093685-1612-4127-8191-025e35a49834.png)  |  ![](https://user-images.githubusercontent.com/76846542/152140719-705758e0-2758-4374-8d75-da8d7113e82c.png)
-
+|                                                   macOs                                                    |                                                  Windows                                                   |
+| :--------------------------------------------------------------------------------------------------------: | :--------------------------------------------------------------------------------------------------------: |
+| ![](https://user-images.githubusercontent.com/76846542/152035198-70093685-1612-4127-8191-025e35a49834.png) | ![](https://user-images.githubusercontent.com/76846542/152140719-705758e0-2758-4374-8d75-da8d7113e82c.png) |
 
 ## Demo Credentials
 
-macOs             |  Windows
-:-------------------------:|:-------------------------:
-![](https://user-images.githubusercontent.com/76846542/152035208-089b3118-d2bb-4ea9-9e6d-34d16438f0e8.png)  |  ![](https://user-images.githubusercontent.com/76846542/152140790-e24bc28c-d23b-4bc3-b553-121a5a75d72d.png)
+|                                                   macOs                                                    |                                                  Windows                                                   |
+| :--------------------------------------------------------------------------------------------------------: | :--------------------------------------------------------------------------------------------------------: |
+| ![](https://user-images.githubusercontent.com/76846542/152035208-089b3118-d2bb-4ea9-9e6d-34d16438f0e8.png) | ![](https://user-images.githubusercontent.com/76846542/152140790-e24bc28c-d23b-4bc3-b553-121a5a75d72d.png) |
--- password.py
+++ password.py
@@ -1,15 +1,98 @@
-from tkinter import *
-from tkinter import messagebox
+"""Password manager using tkinter and json"""
 import json
-import pyperclip
 import random
+from tkinter import (END, Button, Canvas, Entry, Label, PhotoImage, Tk, W, messagebox)
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
-'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '~', '@', '#', '$', '%', '^', '&', '(', ')', '*', '+', '_', '-', '=', '<', '>', '?', '/', '|', '{', '}', '[', ']', ';', ':', ',']
+import pyperclip
+
+letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = [
+    "!",
+    "~",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "(",
+    ")",
+    "*",
+    "+",
+    "_",
+    "-",
+    "=",
+    "<",
+    ">",
+    "?",
+    "/",
+    "|",
+    "{",
+    "}",
+    "[",
+    "]",
+    ";",
+    ":",
+    ",",
+]
+
 
 def password_generator():
+    """Generates a random password with 6-8 letters, 3-5 symbols and 3-5 numbers"""
     nr_letters = random.randint(6, 8)
     nr_symbols = random.randint(3, 5)
     nr_numbers = random.randint(3, 5)
@@ -20,77 +103,95 @@ def password_generator():
     password = "".join(password)
     return password
 
+
 WINDOW_BG = "black"
 FIELD_COLORS = "white"
 FIELD_FONT_COLOR = "green"
 LABEL_COLOR = "white"
 FONT = ("Ubuntu", 15, "bold")
 
+
 def get_password():
+    """Generates a password and copies it to clipboard"""
     password = password_generator()
     pyperclip.copy(password)
     password_entry.delete(0, END)
     password_entry.insert(END, password)
 
+
 def database_manager(new_user_entry):
+    """Manages the database of passwords"""
     try:
-        with open("data.json", mode="r") as old_password_file:
+        with open("data.json", mode="r", encoding="utf-8") as old_password_file:
             password_data = json.load(old_password_file)
     except (FileNotFoundError, json.decoder.JSONDecodeError):
-        with open("data.json", mode="w") as new_password_file:
+        with open("data.json", mode="w", encoding="utf-8") as new_password_file:
             json.dump(new_user_entry, new_password_file, indent=4)
     else:
         password_data.update(new_user_entry)
-        with open("data.json", mode="w") as old_password_file:
+        with open("data.json", mode="w", encoding="utf-8") as old_password_file:
             json.dump(password_data, old_password_file, indent=4)
     finally:
         website_entry.delete(0, END)
         email_entry.delete(0, END)
         password_entry.delete(0, END)
 
+
 def save_password():
+    """Saves the password to the database"""
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please fill all the fields")
     else:
-        is_ok = messagebox.askokcancel(title="Confirm entries", message=f"These are the credentials you entered\n"
-                                                                        f"Email/Username : {email}"
-                                                                        f"\nPassword : {password}\nDo you want to save these ?")
+        is_ok = messagebox.askokcancel(
+            title="Confirm entries",
+            message=f"These are the credentials you entered\n"
+            f"Email/Username : {email}"
+            f"\nPassword : {password}\nDo you want to save these ?",
+        )
         if is_ok:
             pyperclip.copy(password)
-            new_entry_in_json = {
-                website:
-                    {
-                        "Email": email,
-                        "Password": password
-                    }
-            }
+            new_entry_in_json = {website: {"Email": email, "Password": password}}
             database_manager(new_entry_in_json)
 
+
 def search_password():
+    """Searches for the password in the database and copies it to clipboard"""
     website = website_entry.get()
     if len(website) == 0:
         messagebox.showinfo(title="Oops", message="Please enter a website to search")
     else:
         try:
-            with open("data.json", mode="r") as old_password_file:
+            with open("data.json", mode="r", encoding="utf-8") as old_password_file:
                 password_data = json.load(old_password_file)
         except (FileNotFoundError, json.decoder.JSONDecodeError):
-            messagebox.showinfo(title="No passwords saved", message="You have not yet saved any passwords")
+            messagebox.showinfo(
+                title="No passwords saved",
+                message="You have not yet saved any passwords",
+            )
         else:
             if website in password_data:
                 email = password_data[website]["Email"]
                 password = password_data[website]["Password"]
-                is_clipboard = messagebox.askokcancel(title=website, message=f"Email/Username : {email}\nPassword : {password}"
-                                                    f"\n\nSave to clipboard ?")
+                is_clipboard = messagebox.askokcancel(
+                    title=website,
+                    message=f"Email/Username : {email}\nPassword : {password}"
+                    f"\n\nSave to clipboard ?",
+                )
                 if is_clipboard:
                     pyperclip.copy(password)
                     messagebox.showinfo(
-                        title="Saved to clipboard", message="Password has been saved to clipboard")
+                        title="Saved to clipboard",
+                        message="Password has been saved to clipboard",
+                    )
             else:
-                messagebox.showinfo(title="Password not saved for this website", message=f"The password for {website}\n" f"has not been saved")
+                messagebox.showinfo(
+                    title="Password not saved for this website",
+                    message=f"The password for {website}\n" f"has not been saved",
+                )
+
 
 window = Tk()
 window.title("Password Manager")
@@ -105,10 +206,14 @@ canvas.grid(column=1, row=0)
 website_label = Label(text="Website", bg=WINDOW_BG, padx=20, font=FONT, fg=LABEL_COLOR)
 website_label.grid(column=0, row=1, sticky=W)
 
-email_label = Label(text="Email/Username", bg=WINDOW_BG, padx=20, font=FONT, fg=LABEL_COLOR)
+email_label = Label(
+    text="Email/Username", bg=WINDOW_BG, padx=20, font=FONT, fg=LABEL_COLOR
+)
 email_label.grid(column=0, row=2, sticky=W)
 
-password_label = Label(text="Password", bg=WINDOW_BG, padx=20, font=FONT, fg=LABEL_COLOR)
+password_label = Label(
+    text="Password", bg=WINDOW_BG, padx=20, font=FONT, fg=LABEL_COLOR
+)
 password_label.grid(column=0, row=3, sticky=W)
 window.grid_columnconfigure(1, weight=1)
 
@@ -128,7 +233,9 @@ password_entry.grid(column=1, row=3)
 search_button = Button(text="Search", font=FONT, command=search_password)
 search_button.grid(column=5, row=1)
 
-generate_button = Button(text="Generate Strong Password", font=FONT, command=get_password)
+generate_button = Button(
+    text="Generate Strong Password", font=FONT, command=get_password
+)
 generate_button.grid(column=5, row=3)
 
 add_button = Button(text="Save Credentials", font=FONT, command=save_password)
